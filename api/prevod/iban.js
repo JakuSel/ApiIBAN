57f938a64c40ef3e040d89230568b61c37b1dd2d
Jakub Seliga <jakub.seliga@akademiasovy.sk>
2018-11-25 16:30:06 +0100

    update

--- api/prevod/iban.js
+++ api/prevod/iban.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const leftPad = require('left-pad');
 
-router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message:'Handling GET request to /iban'
-    });
-});
-
 router.post('/',(req,res,next)=>{
     const iban ={
         acc: req.body.acc,
@@ -32,39 +26,49 @@ router.post('/',(req,res,next)=>{
             var SK = "282000";
             var checksum = banka + prefix + cisloUctu + SK;
             var checksumFinal = (98 - Modulo(checksum));
-            return "SK".concat(checksumFinal, " ",
-                " ",iban.code," ","0000 00"," ",cisloUctu);
-        }
-
-        function BankaString(iban)
-        {
-            var kodBanky = [ "5200","0200","0900","0720","1100","1111","3000","3100","5900",
-            "6500","7300","7500","7930","8050","8100","8120","8130","8160","8170","8180","8300",
-            "8320","8330","8360","8370","8410"];
-            var nazovBanky = [ "OTP Banka Slovensko, a.s.", "Všeobecná úverová banka, a.s.",
-            "Slovenská sporiteľňa, a.s.","Národná banka Slovenska","Tatra Banka,a.s.",
-                "UniCredit Bank Slovakia,a.s.","Slovenská záručná a rozvojová banka, a.s.",
-            "VOLKSBANK Slovensko, a.s.","Prvá stavebná sporiteľňa, a.s.","Poštová banka, a.s.",
-            "ING Bank N.V., pobočka zahraničnej banky","Československá obchodná banka, a.s.",
-            "Wüstenrot stavebná sporiteľňa, a.s.","COMMERZBANK Aktiengesellschaft, pobočka zahraničnej banky, Bratislava,",
-            "Komerční banka Bratislava, a.s.","Privatbanka, a.s.,","Citibank Europe plc, pobočka zahraničnej banky",
-            "EXIMBANKA SR","ČSOB stavebná sporiteľňa, a. s.","Štátna pokladnica","HSBC Bank plc, pobočka zahraničnej banky",
-            "J&T BANKA, a.s., pobočka zahraničnej banky","Fio banka, a.s. pobočka zahraničnej banky",
-            "mBank S.A, pobočka zahraničnej banky","Oberbank AG pobočka zahraničnej banky v Slovenskej republike",
-            "ZUNO BANK AG, pobočka zahraničnej banky"];
-
-            for(var i =0; i < kodBanky.length; i++)
-            {
-                if (kodBanky[i]===(iban.code))
-                    return nazovBanky[i];
+            if(checksumFinal < 10) {
+                checksumFinal = "0" + checksumFinal;
             }
-            return null;
+            var plainIban = "SK"+checksumFinal+iban.code+"000000"+cisloUctu;
+            return  plainIban.replace(/(\w{4})/g,"$1 ").replace(/(^\s+|\s+$)/," ").trim();
         }
 
+    function BankaString(iban)
+    {
+        var kodBanky = [ "5200","0200","0900","0720","1100","1111","3000","3100","5900",
+        "6500","7300","7500","7930","8050","8100","8120","8130","8160","8170","8180","8300",
+        "8320","8330","8360","8370","8410"];
+        var nazovBanky = [ "OTP Banka Slovensko, a.s.", "Všeobecná úverová banka, a.s.",
+        "Slovenská sporiteľňa, a.s.","Národná banka Slovenska","Tatra Banka,a.s.",
+            "UniCredit Bank Slovakia,a.s.","Slovenská záručná a rozvojová banka, a.s.",
+        "VOLKSBANK Slovensko, a.s.","Prvá stavebná sporiteľňa, a.s.","Poštová banka, a.s.",
+        "ING Bank N.V., pobočka zahraničnej banky","Československá obchodná banka, a.s.",
+        "Wüstenrot stavebná sporiteľňa, a.s.","COMMERZBANK Aktiengesellschaft, pobočka zahraničnej banky, Bratislava,",
+        "Komerční banka Bratislava, a.s.","Privatbanka, a.s.,","Citibank Europe plc, pobočka zahraničnej banky",
+        "EXIMBANKA SR","ČSOB stavebná sporiteľňa, a. s.","Štátna pokladnica","HSBC Bank plc, pobočka zahraničnej banky",
+        "J&T BANKA, a.s., pobočka zahraničnej banky","Fio banka, a.s. pobočka zahraničnej banky",
+        "mBank S.A, pobočka zahraničnej banky","Oberbank AG pobočka zahraničnej banky v Slovenskej republike",
+        "ZUNO BANK AG, pobočka zahraničnej banky"];
+
+        for(var i =0; i < kodBanky.length; i++)
+        {
+            if (kodBanky[i]===(iban.code))
+                return nazovBanky[i];
+        }
+        return null;
+    }
+if (BankaString(iban)!=null && iban.acc.length<11 && iban.code !=4 ){
     res.status(201).json({
         Iban: IbanString(iban), 
         Banka: BankaString(iban)
     });
+}
+else{
+    res.status(404).json({
+        message:"Wrong input"
+    });
+}
+
 });
 
 
